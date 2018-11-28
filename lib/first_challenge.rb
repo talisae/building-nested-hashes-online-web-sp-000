3fe9f1938b076caf91396515f399bd32472f1eaa
Talisa Erazo <talisaerazo@gmail.com>
2018-11-28 17:47:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,40 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+    :montague => {},
+    :capulet => {}
+}
+end
+
+def second_challenge
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+  }
+end
+
+def third_challenge
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :heroine_friends => []
+   }
+  }
 end
