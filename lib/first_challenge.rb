7fd352ca5a4a0a6d5e98c84005cc2036c9ceabff
Talisa Erazo <talisaerazo@gmail.com>
2018-11-28 05:07:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,23 @@
 def first_challenge
-   epic_tragedy = {}
+  epic_tragedy = {
+    :montague => {},
+    :capulet => {}
+}
+end
+
+def second_challenge
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 end
